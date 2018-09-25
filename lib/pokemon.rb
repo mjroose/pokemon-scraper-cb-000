a5b9e1e95fda3fd0e97d3a65ca234c7251431600
Michael Roose <mjroose@gmail.com>
2018-09-25 20:43:56 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  @@all = []
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
