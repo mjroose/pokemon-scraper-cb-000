3a20a0269785ddd15047d36e5fd944e7a1f30fe6
Michael Roose <mjroose@gmail.com>
2018-09-25 20:53:16 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@ class Pokemon
   @@all = []
 
   def initialize(id, name = nil, type = nil, db = nil)
-    @id = :id
-    @name = :name
-    @type = :type
-    @db = :db
+    @id = id
+    @name = name
+    @type = type
+    @db = db
     binding.pry
     @@all << self
   end
